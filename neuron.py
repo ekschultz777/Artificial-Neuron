399702254d41e17e1a163b41458ea0652e17d9dd
Ted Schultz <tedschultz@Teds-MacBook-Pro.local>
2024-09-18 16:30:02 -0700

    Created artificial neuron

---
+++ neuron.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+class Neuron():
+    class Model():
+        def __init__(self, input, output):
+            self.input = input
+            self.output = output
+
+    def __init__(self, model):
+        self.model = model
+        np.random.seed(1)
+        
+        #converting weights to a 3 by 3 matrix with values from -1 to 1 and mean of 0
+        self.synaptic_weights = 2 * np.random.random((3, len(self.model.input[0]))) - 1
+
+    def sigmoid(self, x):
+        # Sigmoid function
+        return 1 / (1 + np.exp(-x))
+
+    def sigmoid_diff(self, x):
+        # derivative to the Sigmoid function
+        return x * (1 - x)
+
+    def train(self, training_iterations):
+        for iteration in range(training_iterations):
+            output = self.output(self.model.input)
+            error = model.output - output
+            adjustment_factors = error * self.sigmoid_diff(output)
+            adjustment_diff = np.dot(self.model.input.T, error * self.sigmoid_diff(output))
+            self.synaptic_weights += adjustment_diff
+
+    def output(self, inputs):
+        inputs = inputs.astype(float)
+        output = self.sigmoid(np.dot(inputs, self.synaptic_weights))
+        return output
+
+
+if __name__ == "__main__":
+        # Guess the bitwise operator << 1
+    model = Neuron.Model(
+        input=np.array([[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0]]), 
+        output=np.array([[0,0,0], [0,1,0], [1,0,0], [1,1,0], [0,0,0], [0,1,0], [1,0,0]])
+    )
+    neuron = Neuron(model=model)
+    neuron.train(150)
+    
+    new_situation = [1, 1, 1]
+    output = neuron.output(np.array(new_situation))
+    # Round the numbers
+    output = [round(x) for x in output] 
+    print(f'Bitwise left (<< 1) guess for: {new_situation} = {output}')
+
+    # Guess the bitwise operator >> 1 
+    model = Neuron.Model(
+        input=np.array([[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0]]), 
+        output=np.array([[0,0,0], [0,0,0], [0,0,1], [0,0,1], [0,1,0], [0,1,0], [0,1,1]])
+    )
+    neuron = Neuron(model=model)
+    neuron.train(150)
+    
+    new_situation = [1, 1, 1]
+    output = neuron.output(np.array(new_situation))
+    # Round the numbers
+    output = [round(x) for x in output]
+    print(f'Bitwise right (>> 1) guess for: {new_situation} = {output}')
+
+    # Guess the opposite (!) operator
+    model = Neuron.Model(
+        input=np.array([[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0]]), 
+        output=np.array([[1,1,1], [1,1,0], [1,0,1], [1,0,0], [0,1,1], [0,1,0], [0,0,1]])
+    )
+    neuron = Neuron(model=model)
+    neuron.train(100)
+    
+    new_situation = [1, 1, 1]
+    output = neuron.output(np.array(new_situation))
+    output = [round(x) for x in output] 
+    print(f'Opposite (!) guess for: {new_situation} = {output}')
+
+    new_situation = [1, 1, 0]
+    output = neuron.output(np.array(new_situation))
+    output = [round(x) for x in output] 
+    print(f'Opposite (!) guess for: {new_situation} = {output}')
+
+    
